078b69d65d18776d11405fd28e11f0082bac910f
cordplace <msk7316@gmail.com>
2023-11-16 08:00:42 +0900

    style:글로벌css 모바일 최적화width적용, main페이지 로고 추가, 헤더 생성 아이콘 추가

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
 import "./globals.css";
+import styles from "./layout.module.css";
+import backArrow from "../../public/icons/back_light_Arrow.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,13 +13,19 @@ export const metadata = {
 
 export const viewport = {
   themeColor: "#11ff58",
+  content: "width=device-width, initial- scale=1.0, user - scalable=no",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="manifest" href="/manifest.json" />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className={styles.header}>
+          <Image src={backArrow} alt={"뒤로가기"}></Image>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
