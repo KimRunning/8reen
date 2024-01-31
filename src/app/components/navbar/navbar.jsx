828b2bb17e28d86275d074064ac80b0ecfdf1903
cordplace <msk7316@gmail.com>
2024-01-31 14:33:31 +0900

    navbar component 로그인 없이 photoShot, myinfo 접근 가능하게 변경

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -9,34 +9,40 @@ function Navbar() {
   const session = useSession().data;
   const router = useRouter();
 
-  const handlePhotoClick = () => {
-    if (!session) {
-      // 세션이 없는 경우, 경고 메시지를 표시하고 로그인 페이지로 이동합니다.
-      alert("로그인이 필요합니다");
-      router.push("/logIn"); // 실제 로그인 페이지 URL로 바꿔주세요.
-    } else {
-      router.push("/photoShot");
-    }
-  };
+  // const handlePhotoClick = () => {
+  //   if (!session) {
+  //     // 세션이 없는 경우, 경고 메시지를 표시하고 로그인 페이지로 이동합니다.
+  //     alert("로그인이 필요합니다");
+  //     router.push("/logIn"); // 실제 로그인 페이지 URL로 바꿔주세요.
+  //   } else {
+  //     router.push("/photoShot");
+  //   }
+  // };
 
-  const handleMyInfoClick = () => {
-    if (!session) {
-      alert("로그인이 필요합니다");
-      router.push("/logIn");
-    } else {
-      router.push(`/myInfo/${session.user.name}`);
-    }
-  };
+  // const handleMyInfoClick = () => {
+  //   if (!session) {
+  //     alert("로그인이 필요합니다");
+  //     router.push("/logIn");
+  //   } else {
+  //     router.push(`/myInfo/${session.user.name}`);
+  //   }
+  // };
 
   return (
     <>
       <section className={styles.navWrap}>
-        <span onClick={handlePhotoClick} className={styles.navbar}>
+        {/* <span onClick={handlePhotoClick} className={styles.navbar}>
           Photo
-        </span>
-        <span onClick={handleMyInfoClick} className={styles.navbar}>
+        </span> */}
+        <Link href="/photoShot" className={styles.navbar}>
+          photo
+        </Link>
+        <Link href="/myInfo" className={styles.navbar}>
           내정보
-        </span>
+        </Link>
+        {/* <span onClick={handleMyInfoClick} className={styles.navbar}>
+          내정보
+        </span> */}
         <Link href="/preparing" className={styles.navbar}>
           Q&A
         </Link>
