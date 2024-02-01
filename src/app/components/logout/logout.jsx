a7628f53ec44de4eb8a7747e7cc7f6e740a79437
cordplace <msk7316@gmail.com>
2024-02-01 23:17:19 +0900

    로그인 없이 포토 샷 페이지, 내 정보 페이지 접근 불가로 변경

--- src/app/components/logout/logout.jsx
+++ src/app/components/logout/logout.jsx
@@ -3,12 +3,15 @@ import { signOut, useSession } from "next-auth/react";
 import React from "react";
 import Link from "next/link";
 import styles from "./logout.module.css";
+import { useRouter } from "next/navigation";
 
 function Logout() {
+  const router = useRouter();
   const { data: session } = useSession();
   const LogOutLogic = () => {
     signOut();
     alert("로그아웃 되었습니다");
+    router.replace("/");
   };
 
   return (
