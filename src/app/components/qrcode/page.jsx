d66d1cb840776be88c2ca1c84950040a65fd4f37
cordplace <msk7316@gmail.com>
2024-02-07 19:25:54 +0900

    feat: QR코드 페이지 리셋

--- src/app/components/qrcode/page.jsx
+++ src/app/components/qrcode/page.jsx
@@ -1,52 +1,78 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { QrReader } from "react-qr-reader";
 import { useSession } from "next-auth/react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 
 function ScanQrCode() {
-  const [data, setData] = useState("QR 코드를 스캔해주세요");
-  const [cameraFacing, setCameraFacing] = useState("environment");
-  const { data: session } = useSession();
-  const router = useRouter();
+  const [cameraFacing, setCameraFacing] = useState("environment"); // 기본값은 'user'
+  const session = useSession().data;
+  const [rented, setRented] = useState(false); // 대여 상태를 관리하는 상태 변수
 
-  const handleQRScan = (result, error) => {
-    if (!!result) {
-      setData(result?.text);
-      // QR 코드 데이터와 사용자 세션 정보를 사용하여 서버에 요청을 보냅니다.
-      // 예시: 대여 또는 반납 처리
-      axios
-        .post("/api/rent/rent", {
-          qrCode: result.text,
-          nickname: session.user.name,
-        })
-        .then(response => {
-          alert("처리 성공");
-          router.push("/myInfo");
-          // 처리 성공 후 리다이렉션 또는 추가 로직
-        })
-        .catch(error => {
-          console.error("처리 실패:", error);
-          alert("처리 중 오류가 발생했습니다.");
-        });
+  useEffect(() => {
+    // 모바일 기기인지 확인
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      // 모바일 기기라면 카메라를 후면 카메라로 설정
+      setCameraFacing("environment");
     }
+  }, []);
 
-    if (!!error) {
-      console.error(error);
+  // 카메라 전환 함수
+  const toggleCamera = () => {
+    setCameraFacing(prevFacing => (prevFacing === "environment" ? "user" : "environment"));
+    console.log(cameraFacing);
+  };
+
+  const handleRentReturn = async qrCode => {
+    try {
+      const response = await axios.post("/api/rent/rent", {
+        nickname: session.user.name,
+        qrCode: qrCode,
+      });
+
+      if (response.status === 200) {
+        setRented(!rented);
+        alert(rented ? "반납 완료" : "대여 완료");
+      }
+    } catch (error) {
+      console.error(rented ? "반납 요청 실패:" : "대여 요청 실패:", error);
     }
   };
 
-  const toggleCamera = () => {
-    setCameraFacing(prevFacing => (prevFacing === "environment" ? "user" : "environment"));
+  const handleQRScan = data => {
+    if (data) {
+      handleRentReturn(data);
+    }
+  };
+
+  const handleErrorQR = err => {
+    console.error("QR 스캔 오류:", err);
   };
 
   return (
-    <div>
-      <h3>{data}</h3>
-      <QrReader delay={300} facingMode={cameraFacing} onResult={handleQRScan} style={{ width: "100%" }} />
-      <button onClick={toggleCamera}>카메라 전환</button>
-    </div>
+    <>
+      <div>
+        <QrReader
+          key={cameraFacing} // 카메라 방향 변경에 따라 키 값이 변경됩니다.
+          delay={300}
+          facingMode={cameraFacing}
+          onError={handleErrorQR}
+          onResult={handleQRScan}
+          style={{ width: "100%" }}
+        />
+
+        <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <button
+            onClick={toggleCamera}
+            style={{ width: "70px", height: "70px", borderRadius: "50%", backgroundColor: "#bdbdbd", border: "transparent", margin: "10px" }}
+          >
+            {cameraFacing === "environment" ? "화면전환" : "화면전환"}
+          </button>
+        </div>
+        <p>QR 코드를 카메라 앞에 위치시켜주세요.</p>
+      </div>
+    </>
   );
 }
 
